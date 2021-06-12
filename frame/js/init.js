c187face8d6eda1b6b583c490b4f48ef7ccce20e
Lam Nguyen <bluelaguna.me@gmail.com>
2021-06-12 10:09:34 +0700

    update frame

--- frame/js/init.js
+++ frame/js/init.js
@@ -18,36 +18,8 @@ document.addEventListener('DOMContentLoaded', () => {
 		});
 	}
 });
-// Tabs
-'use strict';
-function Tabs() {
-	var bindAll = function() {
-		var menuElements = document.querySelectorAll('[data-tab]');
-		for(var i = 0; i < menuElements.length ; i++) {
-		menuElements[i].addEventListener('click', change, false);
-		}
-	}
-	var clear = function() {
-		var menuElements = document.querySelectorAll('[data-tab]');
-		for(var i = 0; i < menuElements.length ; i++) {
-		menuElements[i].classList.remove('is-active');
-		var id = menuElements[i].getAttribute('data-tab');
-		document.getElementById(id).classList.remove('is-active');
-		}
-	}
-	var change = function(e) {
-		clear();
-		e.target.classList.add('is-active');
-		var id = e.currentTarget.getAttribute('data-tab');
-		document.getElementById(id).classList.add('is-active');
-		e.preventDefault();
-	}
-	bindAll();
-}
-var connectTabs = new Tabs();
 // Other
 $(document).ready(function() {
-
 	var $videoSrc;  
 	$('.func-modal-video').click(function() {
 		$videoSrc = $(this).data( "src" );
