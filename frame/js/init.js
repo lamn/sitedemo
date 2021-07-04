e3cd181c4601644ad6e1fe7a58081a4ea18e1dd5
Lam Nguyen <bluelaguna.me@gmail.com>
2021-07-04 22:00:17 +0700

    update test

--- frame/js/init.js
+++ frame/js/init.js
@@ -40,11 +40,13 @@ $(document).ready(function() {
 		$("#video").attr('src',$videoSrc); 
 	});
 });
-function isFacebookApp() {
-	var ua = navigator.userAgent || navigator.vendor || window.opera;
-	return (ua.indexOf("FBAN") > -1) || (ua.indexOf("FBAV") > -1);
-}
-//document.body.innerHTML = isFacebookApp();
-if(isFacebookApp()){
-	$(".modal-fb").addClass("is-active");
-};
+document.addEventListener('DOMContentLoaded', () => {
+	let ua = navigator.userAgent || navigator.vendor || window.opera;
+	if (ua.indexOf('FBAN') > -1 || ua.indexOf('FBAV') > -1 || ua.indexOf('Instagram') > -1) {
+		if (document.querySelector('input[accept]') !== null) {
+			document.querySelectorAll('input[accept]').forEach(el => {
+				el.removeAttribute('accept');
+			});
+		}
+	}
+});
